0f77fb735b055954f4b65332159c5fbd8e8c89a0
Karla <karlaguaa2006@hotmail.com>
2026-01-11 18:09:06 -0500

    Segundo commit

---
+++ ORBITIX/src/main/java/com/mycompany/orbitix/modelo/Vuelo.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.orbitix.modelo;
+
+/**
+ *
+ * @author karla
+ */
+public class Vuelo {
+
+    private String codigo;
+    private Date fecha;
+    private String hora;
+    private Ruta ruta;
+    private Avion avion;
+    private int asientosDisponibles;
+
+    public Vuelo(String codigo, Date fecha, String hora, Ruta ruta, Avion avion) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.ruta = ruta;
+        this.avion = avion;
+        this.asientosDisponibles = avion.getCapacidad();
+    }
+
+    public void disminuirAsientos() {
+        if (asientosDisponibles > 0) {
+            asientosDisponibles--;
+        }
+    }
+
+    // Getters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public Ruta getRuta() {
+        return ruta;
+    }
+
+    public Avion getAvion() {
+        return avion;
+    }
+
+    public int getAsientosDisponibles() {
+        return asientosDisponibles;
+    }
+}
