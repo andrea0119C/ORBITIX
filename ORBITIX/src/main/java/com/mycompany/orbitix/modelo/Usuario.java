ae6fdfdc5747c18c5c2c6da3717b8fab27f751fa
Karla <karlaguaa2006@hotmail.com>
2026-01-11 18:56:43 -0500

    Cuarto commit

--- ORBITIX/src/main/java/com/mycompany/orbitix/modelo/Usuario.java
+++ ORBITIX/src/main/java/com/mycompany/orbitix/modelo/Usuario.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.orbitix.modelo;
-
+import java.util.List;
 /**
  *
  * @author karla
